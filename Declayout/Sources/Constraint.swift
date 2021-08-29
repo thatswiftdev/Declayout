20a34fec97f0df594ead9f79598a1f12e7f1f5f7
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-08-29 09:29:05 +0700

    Change constraint attribute parameter

--- Declayout/Sources/Constraint.swift
+++ Declayout/Sources/Constraint.swift
@@ -13,7 +13,7 @@ public enum Constraint {
         _ attribute: LayoutAttribute.Center,
         from: UIView,
         relation: NSLayoutConstraint.Relation = .equal,
-        to: UIView,
+        to: UIView?,
         multiplier: CGFloat = 1,
         constant: CGFloat = .zero
        )
@@ -22,8 +22,8 @@ public enum Constraint {
         from: UIView,
         _ attribute1: LayoutAttribute.Spacing,
         relation: NSLayoutConstraint.Relation = .equal,
-        to: UIView,
-        _ attribute2: LayoutAttribute.Spacing,
+        to: UIView?,
+        _ attribute2: LayoutAttribute.Spacing?,
         multiplier: CGFloat = 1,
         constant: CGFloat = .zero
        )
@@ -32,8 +32,8 @@ public enum Constraint {
         from: UIView,
         _ attribute1: LayoutAttribute.Dimension,
         relation: NSLayoutConstraint.Relation = .equal,
-        to: UIView,
-        _ attribute2: LayoutAttribute.Dimension,
+        to: UIView?,
+        _ attribute2: LayoutAttribute.Dimension?,
         multiplier: CGFloat = 1,
         constant: CGFloat = .zero
        )
@@ -41,15 +41,15 @@ public enum Constraint {
   var constraint: NSLayoutConstraint {
     switch self {
     case let .spacing(firstView, attribute1, relation, secondView, attribute2, multiplier, constant):
-      return makeConstraint(from: firstView, attribute1.value, relation: relation, to: secondView, attribute2.value, multiplier: multiplier, constant: constant)
+      return makeConstraint(from: firstView, attribute1.value, relation: relation, to: secondView, attribute2?.value ?? .notAnAttribute, multiplier: multiplier, constant: constant)
     case let .center(attribute, firstView, relation, secondView, multiplier, constant):
       return makeConstraint(from: firstView, attribute.value, relation: relation, to: secondView, attribute.value, multiplier: multiplier, constant: constant)
     case let .dimension(firstView, attribute1, relation, secondView, attribute2, multiplier, constant):
-      return makeConstraint(from: firstView, attribute1.value, relation: relation, to: secondView, attribute2.value, multiplier: multiplier, constant: constant)
+      return makeConstraint(from: firstView, attribute1.value, relation: relation, to: secondView, attribute2?.value ?? .notAnAttribute, multiplier: multiplier, constant: constant)
     }
   }
   
-  private func makeConstraint(from firstView: UIView, _ attribute1: NSLayoutConstraint.Attribute, relation: NSLayoutConstraint.Relation = .equal, to secondView: UIView, _ attribute2: NSLayoutConstraint.Attribute, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint {
+  private func makeConstraint(from firstView: UIView, _ attribute1: NSLayoutConstraint.Attribute, relation: NSLayoutConstraint.Relation = .equal, to secondView: UIView?, _ attribute2: NSLayoutConstraint.Attribute, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint {
     return NSLayoutConstraint(item: firstView, attribute: attribute1, relatedBy: relation, toItem: secondView, attribute: attribute2, multiplier: multiplier, constant: constant)
   }
   
